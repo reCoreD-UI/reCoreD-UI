753e950faeee8dda8c1c6dbd7da5d1fd8f297ff5
Sense T <me@sense-t.eu.org>
2024-04-08 13:49:11 +0800

    add domainRemovemodal

---
+++ web/src/components/domains/DomainRemoveModal.vue
@@ -0,0 +1,13 @@
+<template>
+    <NModal :mask-closable="false" :show="show">
+
+    </NModal>
+</template>
+
+<script setup lang="ts">
+import { NModal } from 'naive-ui'
+
+const props = defineProps<{
+    show: boolean
+}>()
+</script>
