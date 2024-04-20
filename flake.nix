8960b778283af09d48f82f6f7d25e4fe3868e414
Sense T <me@sense-t.eu.org>
2024-04-20 11:02:48 +0800

    1

--- flake.nix
+++ flake.nix
@@ -8,26 +8,50 @@
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        name = "recored-ui";
+        version = "v1.0.0";
       in
       {
-        packages = rec {
-          recored-ui = with pkgs; stdenv.mkDerivation rec {
-            name = "recored-ui";
+        defaultPackage = with pkgs; let
+          web = buildNpmPackage {
+            inherit version;
+            pname = name;
+            src = "${self}/web";
+            npmDepsHash = "sha256-e4AYJa0PXhuBRytH4860v6t3DEcQ5awR24HeXRD5pew=";
+          };
+          app = buildGoModule {
+            pname = name;
+            inherit version;
+
             src = self;
-            buildInputs = [
-              go
-              nodejs
+
+            ldflags = [
+              "-s"
+              "-w"
             ];
-            buildPhase = ''
-              cd web && npm i && npm run build && cd ..
-              go get . && go generate ./... && go build . -o recored-ui -ldflags "-s -w"
+
+            configurePhase = ''
+              cp -r ${web}/lib/node_modules/web/dist server
+              export HOME=/tmp/build
             '';
+
             installPhase = ''
               mkdir -p $out/bin
               cp recored-ui $out/bin
             '';
+
+            doCheck = false;
+            deleteVendor = true;
+            proxyVendor = true;
           };
-          default = recored-ui;
+        in
+        stdenv.mkDerivation {
+          name = "${name}-${version}";
+          src = self;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ${app}/bin/recored-ui $out/bin
+          '';
         };
 
         devShell = with pkgs; mkShell {
@@ -68,7 +92,7 @@
                 environment = {
                   RECORED_MYSQL_DSN = cfg.mysql-dsn;
                 };
-                serviceconfig.ExecStart = "${pkgs.recored-ui}/bin/recored-ui server";
+                serviceconfig.ExecStart = "${defaultPackage}/bin/recored-ui server";
               };
             };
           };
